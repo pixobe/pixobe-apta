88cd5da84bbb3a4bdffc9f5bc7c220ba282371a6
pixobe <email@pixobe.com>
2024-09-15 20:58:27 +0800

    Taking backup

--- patterns/hero.php
+++ patterns/hero.php
@@ -13,18 +13,18 @@
 	<!-- wp:group -->
 	<div class="wp-block-group">
 
-		<!-- wp:spacer {"height":"100px"} -->
-		<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- wp:spacer {"height":"50px"} -->
+		<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
 		<!-- /wp:spacer -->
 
-		<!-- wp:site-tagline {"tagName":"main","level":1,"textAlign":"center", "fontFamily":"heading","fontSize":"xx-large","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}}} /-->
+		<!-- wp:site-tagline {"tagName":"main","level":1,"textAlign":"center", "fontFamily":"heading","fontSize":"x-large","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}}} /-->
 
 		<!-- wp:spacer {"height":"25px"} -->
 		<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
 		<!-- /wp:spacer -->
 
-		<!-- wp:heading {"level":2,"textAlign":"center","fontSize":"large"} -->
-		<h2 class="wp-block-heading has-text-align-center has-large-font-size">
+		<!-- wp:heading {"level":2,"textAlign":"center","fontSize":"large" ,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}}} -->
+		<h2 class="wp-block-heading has-text-align-center has-large-font-size" style="font-style:normal;font-weight:400;line-height:1.2">
 			<?php echo esc_html(get_post_meta(get_the_ID(), "tagline", true)); ?>
 		</h2>
 		<!-- /wp:heading -->
